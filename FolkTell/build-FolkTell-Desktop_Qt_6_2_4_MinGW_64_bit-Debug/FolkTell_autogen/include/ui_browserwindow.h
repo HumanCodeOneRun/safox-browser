4d6d67d4da517668e7bbba4ac5c595093e610c90
SC-WSKun <540610423@qq.com>
2022-04-18 19:44:09 +0800

    add traffic light style

--- FolkTell/build-FolkTell-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/FolkTell_autogen/include/ui_browserwindow.h
+++ FolkTell/build-FolkTell-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/FolkTell_autogen/include/ui_browserwindow.h
@@ -12,9 +12,7 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
-#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QToolButton>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -23,41 +21,46 @@ class Ui_BrowserWindow
 {
 public:
     QWidget *centralwidget;
-    QTabWidget *tabWidget;
-    QWidget *tab;
-    QWidget *tab_2;
-    QMenuBar *menubar;
-    QStatusBar *statusbar;
+    QToolButton *closeBtn;
+    QToolButton *minBtn;
+    QToolButton *switchBtn;
 
     void setupUi(QMainWindow *BrowserWindow)
     {
         if (BrowserWindow->objectName().isEmpty())
             BrowserWindow->setObjectName(QString::fromUtf8("BrowserWindow"));
-        BrowserWindow->resize(800, 600);
+        BrowserWindow->resize(3327, 1793);
+        BrowserWindow->setStyleSheet(QString::fromUtf8("border-radius: 0px 0px 8px 8px;\n"
+"background-color: rgba(46, 50, 53, 100);"));
         centralwidget = new QWidget(BrowserWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
-        tabWidget = new QTabWidget(centralwidget);
-        tabWidget->setObjectName(QString::fromUtf8("tabWidget"));
-        tabWidget->setGeometry(QRect(330, 250, 271, 161));
-        tab = new QWidget();
-        tab->setObjectName(QString::fromUtf8("tab"));
-        tabWidget->addTab(tab, QString());
-        tab_2 = new QWidget();
-        tab_2->setObjectName(QString::fromUtf8("tab_2"));
-        tabWidget->addTab(tab_2, QString());
+        closeBtn = new QToolButton(centralwidget);
+        closeBtn->setObjectName(QString::fromUtf8("closeBtn"));
+        closeBtn->setGeometry(QRect(20, 10, 24, 22));
+        closeBtn->setStyleSheet(QString::fromUtf8("background-color: rgba(235, 106, 99, 100);\n"
+"width:20px;\n"
+"height:20px;\n"
+"border-radius:10px;"));
+        closeBtn->setIconSize(QSize(20, 20));
+        minBtn = new QToolButton(centralwidget);
+        minBtn->setObjectName(QString::fromUtf8("minBtn"));
+        minBtn->setGeometry(QRect(60, 10, 24, 22));
+        minBtn->setStyleSheet(QString::fromUtf8("background-color: rgba(241, 195, 78, 100);\n"
+"width:20px;\n"
+"height:20px;\n"
+"border-radius:10px;"));
+        minBtn->setIconSize(QSize(20, 20));
+        switchBtn = new QToolButton(centralwidget);
+        switchBtn->setObjectName(QString::fromUtf8("switchBtn"));
+        switchBtn->setGeometry(QRect(100, 10, 24, 22));
+        switchBtn->setStyleSheet(QString::fromUtf8("background-color: rgba(57, 194, 77, 100);\n"
+"width:20px;\n"
+"height:20px;\n"
+"border-radius:10px;"));
+        switchBtn->setIconSize(QSize(20, 20));
         BrowserWindow->setCentralWidget(centralwidget);
-        menubar = new QMenuBar(BrowserWindow);
-        menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 22));
-        BrowserWindow->setMenuBar(menubar);
-        statusbar = new QStatusBar(BrowserWindow);
-        statusbar->setObjectName(QString::fromUtf8("statusbar"));
-        BrowserWindow->setStatusBar(statusbar);
 
         retranslateUi(BrowserWindow);
-
-        tabWidget->setCurrentIndex(0);
-
 
         QMetaObject::connectSlotsByName(BrowserWindow);
     } // setupUi
@@ -65,8 +68,9 @@ public:
     void retranslateUi(QMainWindow *BrowserWindow)
     {
         BrowserWindow->setWindowTitle(QCoreApplication::translate("BrowserWindow", "BrowserWindow", nullptr));
-        tabWidget->setTabText(tabWidget->indexOf(tab), QCoreApplication::translate("BrowserWindow", "Tab 1", nullptr));
-        tabWidget->setTabText(tabWidget->indexOf(tab_2), QCoreApplication::translate("BrowserWindow", "Tab 2", nullptr));
+        closeBtn->setText(QString());
+        minBtn->setText(QString());
+        switchBtn->setText(QString());
     } // retranslateUi
 
 };
