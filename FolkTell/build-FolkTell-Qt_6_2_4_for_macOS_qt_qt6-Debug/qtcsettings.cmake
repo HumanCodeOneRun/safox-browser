ffcd21e220c5540e3e90f2905535db569b19756d
StevenWong12 <stevenhuang12@outlook.com>
2022-04-21 17:29:39 +0800

    fixed little bugs

--- FolkTell/build-FolkTell-Qt_6_2_4_for_macOS_qt_qt6-Debug/qtcsettings.cmake
+++
@@ -1,8 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/Users/stevenwong/Qt/Qt Creator.app/Contents/Resources/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/Users/stevenwong/Qt/6.2.4/macos/bin/qmake" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/Users/stevenwong/Qt/6.2.4/macos" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
