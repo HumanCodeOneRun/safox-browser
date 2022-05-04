f9df1a95078331cc76fc4bd9728be9d3a5deb592
thousandyoung <muji_cullin@qq.com>
2022-05-04 15:30:10 +0800

    init

---
+++ tests/user_test.cpp
@@ -0,0 +1,15 @@
+#include "src/user/usermodel.h"
+
+bool t_getItemById() {
+    UserModel um;
+
+}
+bool t_addUsr() {
+    UserModel um;
+    return 1;
+    // um.addRegisterUser("usr2", "123");
+    // return 0 || !um.addRegisterUser("usr1", "");
+}
+int main(int argc, char** argv){
+   return 0 || t_addUsr();
+}
