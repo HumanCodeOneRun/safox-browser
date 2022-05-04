9f94004d2adee7207ed950a5646b956f668270db
thousandyoung <muji_cullin@qq.com>
2022-05-04 15:37:44 +0800

    add dao

---
+++ src/dao/userdao.cpp
@@ -0,0 +1,190 @@
+#include "userdao.h"
+
+UserDao::UserDao(const QString& _db_path, const QString& _table_name )
+: BaseDao(_db_path, _table_name){
+    createTable();
+
+}
+UserDao& UserDao::getDao(){
+    static UserDao dao;
+    return dao;
+}
+bool UserDao::createTable(){
+    QString cmd = "CREATE TABLE IF NOT EXISTS " + this->getTableName() +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, PASSWORD TEXT);";
+    QSqlQuery query(this->db);
+    bool ok = query.exec(cmd);
+
+    if(!ok){
+        qDebug() << "[error] fail to create User table " << query.lastError().text();
+        return false;
+    }
+
+    return true;
+}
+
+
+QVector<QVariant> UserDao::QueryById(const int& id){
+    QString cmd = "SELECT * from " + this->getTableName() + " WHERE ID="+QString::number(id);
+    QSqlQuery query(this->db);
+    query.prepare(cmd);
+    
+    QVector<QVariant> ret;
+    if(!query.exec()){
+        qDebug() << "[error] fail to query by id,  " << query.lastError().text();
+        return ret;
+    }
+    
+    while(query.next()){
+        ret.append({query.value(0), query.value(1),query.value(2)});
+    }
+
+    return ret;
+}
+
+QVector<QVariant> UserDao::QueryByIdPassword(const int& id, const QString& password){
+    QString cmd = "SELECT * from " + this->getTableName() + " WHERE ID="+QString::number(id) + " AND PASSWORD="+password;
+    QSqlQuery query(this->db);
+    query.prepare(cmd);
+    
+    QVector<QVariant> ret;
+    if(!query.exec()){
+        qDebug() << "[error] fail to query by id and password,  " << query.lastError().text();
+        return ret;
+    }
+    
+    while(query.next()){
+        ret.append({query.value(0), query.value(1),query.value(2)});
+    }
+
+    return ret;
+}
+
+QVector<QVector<QVariant>> UserDao::QueryByName(const QString& name){
+    QString cmd = "SELECT * from " + this->getTableName() + " WHERE NAME="+name;
+    QSqlQuery query(this->db);
+    query.prepare(cmd);
+
+    QVector<QVector<QVariant>> ret;
+    if(!query.exec()){
+        qDebug() << "[error] fail to query by name,  " << query.lastError().text();
+        return ret;
+    }
+
+    while(query.next()){
+        QVector<QVariant> temp = {query.value(0), query.value(1),query.value(2)};
+        ret.emplace_back(temp);
+    }
+
+    return ret;
+}
+
+bool UserDao::setName(const int& id, const QString& name){
+    QString cmd = "UPDATE " + this->table_name + "SET NAME=:name WHERE ID=:id";
+    QSqlQuery query(this->db);
+    
+    if(!query.prepare(cmd)){
+        qDebug() << "[error] fail to prepare cmd in setname: " << query.lastError().text();
+        return false;
+    }
+
+    query.bindValue(":name", name);
+    query.bindValue(":id", id);
+
+    if(!query.exec()){
+        qDebug() << "[error] fail to exec cmd in setname: " << query.lastError().text();
+        return false;
+    }
+
+    return true;
+}
+
+bool UserDao::setPassword(const int& id, const QString& password){
+    QString cmd = "UPDATE " + this->table_name + "SET PASSWORD=:password WHERE ID=:id";
+    QSqlQuery query(this->db);
+    
+    if(!query.prepare(cmd)){
+        qDebug() << "[error] fail to prepare cmd in setpassword: " << query.lastError().text();
+        return false;
+    }
+
+    query.bindValue(":password", password);
+    query.bindValue(":id", id);
+
+    if(!query.exec()){
+        qDebug() << "[error] fail to exec cmd in setpassword: " << query.lastError().text();
+        return false;
+    }
+
+    return true;
+}
+
+
+bool UserDao::insert(const QString& name, const QString& password){
+    QString cmd = "INSERT INTO " + this->getTableName() + "( NAME, PASSWORD)"+" VALUES( :name, :password)";
+
+    QSqlQuery query(this->db);
+    query.prepare(cmd);
+    // query.bindValue(":id", id);
+    query.bindValue(":name", name);
+    query.bindValue(":password", password);
+
+    if(!query.exec()){
+        qDebug() << "[error] fail to insert,  " << query.lastError().text();
+        return false;
+    }
+
+    return true;
+}
+
+bool UserDao::remove(const int& id){
+    QString cmd = "DELETE FROM " + this->table_name + " WHERE ID=:id";
+
+    QSqlQuery query(this->db);
+    query.prepare(cmd);
+    query.bindValue(":id", id);
+
+    if(!query.exec()){
+        qDebug() << "[error] fail to delete,  " << query.lastError().text();
+        return false;
+    }
+
+    return true;
+}
+
+QVector<QVariant> UserDao::getcolumns(){
+    QString cmd = "PRAGMA  table_info("+this->table_name+")";
+    QSqlQuery query(this->db);
+    query.prepare(cmd);
+    
+    QVector<QVariant> ret;
+    if(!query.exec()){
+        qDebug() << "[error] fail to query all columns,  " << query.lastError().text();
+        return ret;
+    }
+    
+    while(query.next()){
+        ret.append({query.value(0), query.value(1), query.value(2)});
+    }
+
+    return ret;
+}
+
+bool UserDao::deleteTable(){
+    QString cmd = "DROP TABLE " + this->table_name;
+    
+    QSqlQuery query(this->db);
+    query.prepare(cmd);
+
+    if(!query.exec()){
+        qDebug() << "[error] fail to delete table,  " << query.lastError().text();
+        return false;
+    }
+
+    return true;
+}
+
+
+
+UserDao::~UserDao(){
+}
---
+++ src/dao/userdao.h
@@ -0,0 +1,27 @@
+#include "basedao.h"
+#define USERDAO_TABLE_NAME "User"
+
+class UserDao : public BaseDao{
+public:
+    explicit UserDao(const QString& _db_path=DEFAULT_DB_PATH, const QString& _table_name = USERDAO_TABLE_NAME);
+    
+    static UserDao& getDao();
+    bool createTable() override;
+
+    // query
+    QVector<QVariant> QueryById(const int& id);
+    QVector<QVector<QVariant>> QueryByName(const QString& name);
+    QVector<QVariant> QueryByIdPassword(const int& id, const QString& password);
+    // update
+    bool setName(const int& id, const QString& name);
+    bool setPassword(const int& id, const QString& password);
+    // insert
+    bool insert(const QString& name, const QString& password);
+
+    // delete
+    bool remove(const int& id);
+    // debug
+    QVector<QVariant> getcolumns();
+    bool deleteTable();
+    ~UserDao();
+};
