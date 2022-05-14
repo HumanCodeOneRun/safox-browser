f5e7f6521bbc87ef2f70de365fe48e8b57aa1b9f
SC-WSKun <540610423@qq.com>
2022-05-14 11:49:49 +0800

    combine bookmarkmodel to bookmarkWidget

--- src/bookmark/bookmarkwidget.h
+++ src/bookmark/bookmarkwidget.h
@@ -9,6 +9,11 @@
 #include <QGroupBox>
 #include <QStyleOption>
 #include <QPainter>
+#include <QVector>
+#include <QVariant>
+#include "bookmarkmodel.h"
+#include "bookmarkitem.h"
+#include "../browser.h"
 
 namespace Ui {
 class BookmarkWidget;
@@ -21,12 +26,15 @@ class BookmarkWidget : public QWidget
 public:
     explicit BookmarkWidget(QWidget *parent = nullptr,int x=0,int y=0,int width=0,int height=0);
     int width,height;
+    BookmarkModel* bookmark;
+    QVector<QVector<QVariant>> userBookmark;
     ~BookmarkWidget();
 
 private:
     Ui::BookmarkWidget *ui;
     QLineEdit* searchKey;
     void paintEvent(QPaintEvent *event);
+    void loadBookmarker();
 
 };
 
--- src/browser.cpp
+++ src/browser.cpp
@@ -1,7 +1,7 @@
 #include "browser.h"
 
-Browser::Browser(QObject *parent)
+Browser::Browser(QObject *parent,int userid)
     : QObject{parent}
 {
-
+    this->userid = userid;
 }
